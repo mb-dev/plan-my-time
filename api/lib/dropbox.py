fcd844174b72c5ef1c9ec0da00b7a0f7ea3aac38
Moshe Bergman <bergmanm@gmail.com>
2016-01-25 15:43:41 -0800

    improve dropbox toggle

--- api/lib/dropbox.py
+++ api/lib/dropbox.py
@@ -1,6 +1,6 @@
 from app                  import app
 
-if app.config["DROPBOX_USE_FAKE"]:
-  from lib.dropbox_fake     import DropboxApi
+if app.config["DROPBOX_USE_FAKE"] is True:
+  from lib.dropbox_fake          import DropboxApi
 else:
   from lib.dropbox_real          import DropboxApi
