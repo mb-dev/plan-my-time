1e84f2b6c84b43410e3c0b9e81441efa5194c434
Moshe Bergman <mb-dev@users.noreply.github.com>
2016-02-29 19:22:39 -0800

    better logging

--- api/service.py
+++ api/service.py
@@ -3,4 +3,8 @@ import controllers
 import logging
 
 app.logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
 app.run(debug = app.config["APP_DEBUG"], port = app.config["PORT"])
