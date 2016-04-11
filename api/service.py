0271882f325fee52268b0abe5553b6ad91cede2d
Moshe Bergman <mb-dev@users.noreply.github.com>
2016-04-10 17:45:09 -0700

    attempt to add task scheduler

--- api/service.py
+++ api/service.py
@@ -1,7 +1,12 @@
 from app import app
 import controllers
 import logging
+from apscheduler.schedulers.background import BackgroundScheduler
 
+# all tasks
+import tasks.extract_tags
+
+# configuration
 if not app.config["APP_DEBUG"]:
   app.logger.setLevel(logging.INFO)
   formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -9,5 +14,8 @@ if not app.config["APP_DEBUG"]:
   handler.setFormatter(formatter)
   app.logger.addHandler(handler)
 
+# start service
+scheduler = BackgroundScheduler()
+scheduler.add_job(tasks.extract_tags.run, 'cron', minute=0, hour=1)
 app.run(host='0.0.0.0', debug=app.config["APP_DEBUG"], port = app.config["PORT"])
 
--- api/tasks/extract_tags.py
+++ api/tasks/extract_tags.py
@@ -1,11 +1,8 @@
 import os
 import sys
-import shutil
-
 script_path = __file__
 if os.path.islink(__file__):
-  script_path = os.path.realpath(script_path)
-
+    script_path = os.path.realpath(script_path)
 sys.path.insert(0, os.path.join(os.path.dirname(script_path), "../"))
 
 from bson.code import Code
@@ -20,12 +17,17 @@ def mapper(results, entry):
     for person in entry["metadata"]["people"]:
         results[person] = 'person'
 
-mongo.db.tags.delete_many({}) 
-for user in mongo.db.users.find():
-    results = {}
-    for entry in mongo.db.entries.find({"user_id": user["_id"]}):
-        mapper(results, entry)
 
-    tags = [{"user_id": user["_id"], "tag": tag, "type": type} for tag,type in results.items()]
+def run():
+    mongo.db.tags.delete_many({})
+    for user in mongo.db.users.find():
+        results = {}
+        for entry in mongo.db.entries.find({"user_id": user["_id"]}):
+            mapper(results, entry)
 
-    mongo.db.tags.insert_many(tags)
+        tags = [{"user_id": user["_id"], "tag": tag, "type": type} for tag, type in results.items()]
+
+        mongo.db.tags.insert_many(tags)
+
+if __name__ == "__main__":
+    run()
